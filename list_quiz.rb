77c82e778742a1d72525dbf5f483c61cbc9f30b8
ashinde8513 <56264646+ashinde8513@users.noreply.github.com>
2019-10-08 09:19:11 -0600

    Add files via upload

---
+++ list_quiz.rb
@@ -0,0 +1,32 @@
+def three_even?(list)
+    count = 0 
+    
+
+
+end 
+
+puts three_even?([2,4,6,5]) #true
+puts three_even?([2,3,4,5]) #false 
+puts three_even?([3,4,4,4]) #true
+
+
+
+
+
+# def bigger_two(list)
+use size 
+# end 
+
+# puts bigger_two([1,2], [3,4]) #[3,4]
+# puts bigger_two([1,7], [4,4]) #[1,7]
+# puts bigger_two([3,4], [5,2]) #[3,4]
+
+
+# def series_up(list)
+use push 
+# end 
+
+# puts series_up(1) #[1]
+# puts series_up(2) #[1,1,2]
+# puts series_up(3) #[1,1,2,1,2,3]
+# puts series_up(4) #[1,1,2,1,2,3,1,2,3,4]
